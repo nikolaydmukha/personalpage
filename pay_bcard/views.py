6721de34d90dd63b58d0839ebdff22f316c06260
Nikolay Dmukha <dmukha@mail.ru>
2019-05-11 08:55:27 +0300

    Ready html&css for pay-Bcard

--- pay_bcard/views.py
+++ pay_bcard/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 
 
 def index(request):
-    context = {"active": "bcard", "title": "Пополнить счёт"}
+    context = {"active": "pay_bcard", "title": "Пополнить счёт"}
     return render(request, 'pay_bcard/pay_bcard.html', context)
