b7e15b139cf0e20bd6d69daaead16410d6ac6162
Nikolay Dmukha <dmukha@mail.ru>
2019-07-13 12:48:25 +0300

    Editing block, dpay, pay_bcard for dynamic style

---
+++ pay_bcard/templatetags/personalpage_filters.py
@@ -0,0 +1,15 @@
+from django import template
+register = template.Library()
+
+
+@register.filter(name='phone_number')
+def phone_number(number):
+    """Convert a +79991001111 string into +7(xxx)xxx-xx-xx."""
+    print("@@@@@@@22222", number)
+    country_code = number[0:2]
+    first = number[2:5]
+    print(first,"\n")
+    second = number[5:8]
+    third = number[8:10]
+    fourth = number[10:12]
+    return f'{country_code} ({first}) {second}-{third}-{fourth}'
